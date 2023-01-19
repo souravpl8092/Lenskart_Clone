aad6327a70171bd57d70bd4a40c1af8c9a570ef7
raveena <raveenar.kale@gmail.com>
2023-01-19 21:43:01 +0530

    files added

--- lenskart_clone/Frontend/src/App.js
+++ lenskart_clone/Frontend/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import AllRoutes from "./Pages/AllRoutes";
+import CartNavbar from "./Pages/Cart/CartNavbar.jsx";
+import CartLength from "./Pages/Cart/CartLength";
+import CartItem from "./Pages/Cart/CartItem";
 
 function App() {
   return (
-    <div className="App">
-       <AllRoutes/>
+    <div>
+     
+      <AllRoutes/>
     </div>
   );
 }
