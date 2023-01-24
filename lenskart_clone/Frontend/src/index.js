06f5f6d5f799bbd927f1417f08827a0f60b1d30b
raveena <raveenar.kale@gmail.com>
2023-01-25 01:41:51 +0530

    updated

--- lenskart_clone/Frontend/src/index.js
+++ lenskart_clone/Frontend/src/index.js
@@ -6,30 +6,19 @@ import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./ContextApi/AuthContext";
 import App from "./App";
 import { Provider } from "react-redux";
-import {store} from "./redux/store"
-import CartContext from "./ContextApi/CartContext";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
   <AuthProvider>
     <BrowserRouter>
       <ChakraProvider>
-      <CartContext>
-      <Provider store={store}>
-        <App />
-<<<<<<< HEAD
+        <Provider store={store}>
+          <App />
         </Provider>
-      </CartContext>
-    </ChakraProvider>
-  </BrowserRouter>
-=======
-        </CartContext>
       </ChakraProvider>
     </BrowserRouter>
   </AuthProvider>
-
->>>>>>> d86bf09918789ce192bd08427630bccf8f1ad57a
 );
 
 reportWebVitals();
