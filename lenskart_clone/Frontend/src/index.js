353c6c703834ad9cce255156eb9314b9a9894b99
raveena <raveenar.kale@gmail.com>
2023-01-25 16:42:37 +0530

    update

--- lenskart_clone/Frontend/src/index.js
+++ lenskart_clone/Frontend/src/index.js
@@ -7,15 +7,18 @@ import AuthProvider from "./ContextApi/AuthContext";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import CartContext from "./ContextApi/CartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthProvider>
     <BrowserRouter>
       <ChakraProvider>
+      <CartContext>
         <Provider store={store}>
           <App />
         </Provider>
+        </CartContext>
       </ChakraProvider>
     </BrowserRouter>
   </AuthProvider>
